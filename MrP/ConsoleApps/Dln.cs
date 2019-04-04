52406496933b470939ffdfa1f75aa8ac07b0a6b5
MRobb <mrobb@SouthBridgeLight.com>
2019-04-04 16:42:14 -0400

    Adding DLN CommandApp

---
+++ MrP/ConsoleApps/Dln.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MrP.ConsoleApps
+{
+    public class Dln : AConsoleImplementation
+    {
+        private string PlayerFile;
+
+        public Dln() : base("dln")
+        {
+        }
+
+        public override CommandActionEnum Run(string[] args)
+        {
+            
+
+            return CommandActionEnum.Continue;
+        }
+
+        private CommandActionEnum Players(string[] args)
+        {
+
+        }
+    }
+}
